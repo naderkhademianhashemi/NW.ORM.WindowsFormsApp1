a8e85ad9c02d2d3cc73f2896eb24b92d43aaebff
nader <nhashemi6@gmail.com>
2023-11-02 15:29:59 +0330

    Add project files.

---
+++ NW.ORM.WindowsFormsApp1/Form1.cs
@@ -0,0 +1,29 @@
+﻿using Arian.NorthW.WindowsFormsApp1.REPO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NW.ORM.WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var CATS = CatRepo.Get();
+            var CT = CatRepo.Get(1);
+            //var id = new CatRepo() { CategoryName = "clothes", Description = "..." }.Insert();
+            var I = new CatRepo() { CategoryID = 9, CategoryName = "clothes ... ", Description = "123" }.Update();
+        }
+    }
+}
---
+++ NW.ORM.WindowsFormsApp1/REPO/CatRepo.cs
@@ -0,0 +1,34 @@
+﻿using Arian.NorthW.WindowsFormsApp1.Models;
+using PayamHannan.Arian.ORM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arian.NorthW.WindowsFormsApp1.REPO
+{
+    public class CatRepo
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public static List<Category> Get() => DBConnections.Context.Select<Category>("*").From("Categories").QueryMany();
+
+        public static Category Get(int id)
+        => DBConnections.Context.Sql(
+            @"  SELECT * from Categories
+	                    WHERE   (CategoryID = @id) ")
+            .Parameter("id", id)
+            .QuerySingle<Category>();
+
+        public int Insert() => 
+            CategoryID = DBConnections.Context.Insert("Categories", this).AutoMap(k => k.CategoryID).ExecuteReturnLastId<int>();
+
+        public int Update()
+        => CategoryID = DBConnections.Context.Update("Categories", this)
+            .AutoMap(k => k.CategoryID)
+            .Where(k => k.CategoryID)
+            .Execute();
+    }
+}
